67aad88a71540873086cdf17b37fe73e62041ecd
EduardoBrandao <eduardo.loureiro@madeinweb.app>
2023-10-23 16:35:20 -0300

    ajustando e criando novos componentes novos

---
+++ contas/contaCorrente.go
@@ -0,0 +1,42 @@
+package contas
+
+import "banco/clientes"
+
+type ContaCorrente struct {
+	Titular       clientes.Titular
+	NumeroAgencia int
+	NumeroConta   int
+	Saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+
+	if podeSacar {
+		c.Saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.Saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.Saldo
+	} else {
+		return "Saldo insuficiente", c.Saldo
+	}
+
+}
+
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) string {
+	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
+		c.Saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+
+		return "Transferencia realizada com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
--- main.go
+++ main.go
@@ -1,34 +1,38 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular string
-	numeroAgencia int
-	numeroConta int
-	saldo float64
-} 
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
+import (
+	"banco/clientes"
+	"banco/contas"
+	"fmt"
+)
 
 func main() {
 
-	contaEduardo := ContaCorrente{}
-	contaEduardo.titular = "Eduardo"
-	contaEduardo.saldo = 500
+	contaBeatriz := contas.ContaCorrente{Titular: clientes.Titular{
+		Nome: "Beatriz",
+		CPF: "123.111.123.11",
+		Profissao: "Enfermeira"},
+	NumeroAgencia: 123, NumeroConta: 123456, Saldo: 1200}
 
-	fmt.Println(contaEduardo.saldo)
-	 
-	fmt.Println(contaEduardo.Sacar(300))
-
-	fmt.Println(contaEduardo.saldo)
+	fmt.Println(contaBeatriz)
 }
+
+//contaEduardo := contas.ContaCorrente{Titular: "Eduardo", Saldo: 500}
+	//contaCarlos := contas.ContaCorrente{Titular: "Carlos", Saldo: 800}
+
+	//status := contaCarlos.Transferir(500, &contaEduardo)
+
+	//fmt.Println(status)
+	//fmt.Println(contaCarlos)
+	//fmt.Println(contaEduardo)
+
+	//contaEduardo := ContaCorrente{}
+	//contaEduardo.titular = "Eduardo"
+	//contaEduardo.saldo = 500
+
+	//fmt.Println(contaEduardo.Sacar(300))
+	//contaEduardo.Depositar(400)
+	//fmt.Println(contaEduardo.saldo)
+	//fmt.Println(contaEduardo.Depositar(400))
+	//status, valor := contaEduardo.Depositar(400)
+	//fmt.Println(status, valor)
